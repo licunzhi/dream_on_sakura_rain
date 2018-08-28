7ab24fd8c856eb84075573f5fe3675600c66a4f7
licunzhi <licunzhi@trendytech.com.cn>
2018-08-28 22:38:49 +0800

    design pattern - singleton pattern

---
+++ java_design_model_demo/src/main/java/com/sakura/SingletonPattern/mode/SingletonLazy.java
@@ -0,0 +1,32 @@
+package com.sakura.SingletonPattern.mode;
+
+/**
+ * @author licunzhi
+ * @desc lazy mode not safe
+ * @date 2018-08-28
+ */
+public class SingletonLazy {
+
+    private static SingletonLazy instance;
+
+    /**
+     * private constructor avoid instanced
+     */
+    private SingletonLazy() {
+
+    }
+
+    public static SingletonLazy getInstance() {
+
+        if (instance == null) {
+            /**
+             * there may not create a instance and another object use this method to create instance
+             *
+             * so the method is not a atom operation
+             */
+            instance = new SingletonLazy();
+        }
+        return instance;
+
+    }
+}
