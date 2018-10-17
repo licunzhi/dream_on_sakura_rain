1cf76c9bbcebe254949e1a6a4494f905b3c3e06f
licunzhi <licunzhi@trendytech.com.cn>
2018-10-17 10:11:52 +0800

    提交10 16 总结信息

---
+++ java_face_demo/src/main/java/com/sakura/day_10_16/DemoTest.java
@@ -0,0 +1,32 @@
+package com.sakura.day_10_16;
+
+/**
+ * @author licunzhi
+ * @desc 描述功能
+ * @date 2018-10-16
+ */
+public class DemoTest {
+    public static void main(String[] args) {
+        /*
+        * 面向对象特征
+        *       封装
+        *       继承
+        *       多态
+        * */
+
+
+        /*
+        * 修饰符
+        *       public 不同包文件 不同类文件
+        *       protected 类 或继承类中调用
+        *       private 同类中调用
+        * */
+
+        /*
+        *  final
+        *  finally
+        *  finalize
+        * */
+    }
+
+}
---
+++ springboot-redis-demo/src/test/java/com/meimie/ApplicationTests.java
@@ -0,0 +1,26 @@
+package com.meimie;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author licunzhi
+ * @desc 描述功能
+ * @date 2018-10-16
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+
+}
