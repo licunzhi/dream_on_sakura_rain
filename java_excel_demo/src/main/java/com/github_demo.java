92e5f2aef459211752a3c11906316ca97df849ad
licunzhi <2718272293@qq.com>
2018-10-27 22:46:55 +0800

    统计分析解除压缩实现方式
    改用采用混合下载实现

--- java_excel_demo/src/main/java/com/github_demo.java
+++ java_excel_demo/src/main/java/com/github_demo.java
@@ -5,7 +5,9 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class github_demo {
@@ -26,6 +28,14 @@ public class github_demo {
         FileInputStream fileInputStream = new FileInputStream(filePath);
         HSSFWorkbook hssfWorkbook = new HSSFWorkbook(fileInputStream);
         HSSFWorkbook result = ExcelUtils.createPieChart(hssfWorkbook, "虚拟机使用率", dataMap, "图表");
+
+        //数据指标
+        Map<String, List<String>> dataMapSheet = new LinkedHashMap<>();
+        dataMapSheet.put("1", Arrays.asList("qqq","qqqqq","qqqqq","qqqqqq"));
+        dataMapSheet.put("2", Arrays.asList("www","wwww","wwwww","wwwwww"));
+        dataMapSheet.put("3", Arrays.asList("aaa","aaaa","aaaaa","aaaaaa"));
+        dataMapSheet.put("4", Arrays.asList("ccc","cccc","ccccc","cccccc"));
+        result = ExcelUtils.createDataSheet(result, "虚拟机使用率", dataMapSheet, "0-20%");
         FileOutputStream fileOutputStream = new FileOutputStream(outFile);
         result.write(fileOutputStream);
         System.out.println("输出文件 " + outFile);
