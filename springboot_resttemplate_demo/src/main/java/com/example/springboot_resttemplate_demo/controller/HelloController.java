dbe4b7d86526cec84b477e6c0c89c66232473351
licunzhi <licunzhi@trendytech.com.cn>
2018-11-07 10:45:12 +0800

    dockerfile文件模板

--- springboot_resttemplate_demo/src/main/java/com/example/springboot_resttemplate_demo/controller/HelloController.java
+++ springboot_resttemplate_demo/src/main/java/com/example/springboot_resttemplate_demo/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -45,5 +46,21 @@ public class HelloController {
         user.setEmail("1205131711@qq.com");
         ResponseEntity<User> responseEntity = new ResponseEntity<>(user, HttpStatus.OK);
         return responseEntity;
+    }
+
+    @GetMapping("/weather/{city}")
+    public String weather(@PathVariable(value = "city") String city) {
+        String apiUrl = "http://wthrcdn.etouch.cn/weather_mini?city=" + city;
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
+        String body = responseEntity.getBody();
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        int statusCodeValue = responseEntity.getStatusCodeValue();
+        HttpHeaders headers = responseEntity.getHeaders();
+        StringBuffer result = new StringBuffer();
+        result.append("responseEntity.getBody()：").append(body).append("<hr>").append("responseEntity.getStatusCode()：")
+                        .append(statusCode).append("<hr>").append("responseEntity.getStatusCodeValue()：")
+                        .append(statusCodeValue).append("<hr>").append("responseEntity.getHeaders()：").append(headers)
+                        .append("<hr>");
+        return result.toString();
     }
 }
