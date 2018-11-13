3f1c685238cbcc538f5c28b82768972d925a6053
licunzhi <2718272293@qq.com>
2018-11-13 21:31:10 +0800

    前后端部分项目测试

---
+++ springboot-html/src/main/java/com/example/springboothtml/SpringbootHtmlApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboothtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHtmlApplication.class, args);
+	}
+}
