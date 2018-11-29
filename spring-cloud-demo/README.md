4a365468411a2ca9587cfb57137cfebb63a1149b
licunzhi <licunzhi@trendytech.com.cn>
2018-11-29 12:39:47 +0800

    搭建一个消费者和生产者 初始话到成功搭建真几吧难

---
+++ spring-cloud-demo/README.md
@@ -0,0 +1,1 @@
+### 这是一个springcloud的项目demo
---
+++ spring-cloud-demo/cloud-user-service-demo/src/main/java/com/sakura/rain/CloudUserServiceDemoApplication.java
@@ -0,0 +1,16 @@
+package com.sakura.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author licunzhi
+ * @desc 启动类
+ * @date 2018-11-29
+ */
+@SpringBootApplication
+public class CloudUserServiceDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudUserServiceDemoApplication.class, args);
+    }
+}
