dff4c4ada476b44a347a779e5a09f82019c6818f
licunzhi <licunzhi@trendytech.com.cn>
2018-11-29 19:31:20 +0800

    maven完善和修正导包重复问题

---
+++ spring-cloud-demo/cloud-user-eureka-demo/src/main/java/com/sakura/rain/CloudEurekaApplication.java
@@ -0,0 +1,18 @@
+package com.sakura.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author licunzhi
+ * @desc 描述功能
+ * @date 2018-11-29
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+}
