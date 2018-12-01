efb10600911d28b075fbdbfcbefc6690c8f3cf8d
licunzhi <licunzhi@trendytech.com.cn>
2018-12-01 23:03:53 +0800

    如何使用ribbon内部定义的负载策略

--- spring-cloud-demo/cloud-user-consumer-demo/src/main/java/com/sakura/rain/configuration/ConfigurationBean.java
+++ spring-cloud-demo/cloud-user-consumer-demo/src/main/java/com/sakura/rain/configuration/ConfigurationBean.java
@@ -1,5 +1,7 @@
 package com.sakura.rain.configuration;
 
+import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,5 +19,12 @@ public class ConfigurationBean {
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public AbstractLoadBalancerRule getBalanceRule() {
+        return new RoundRobinRule();//默认机制
+        //return new RandomRule();//随机策略
+        //return new RetryRule();//默认轮训的机制上有纠错的恢复功能
     }
 }
