cfb3a9dd6b776fd85a4ca1d6b9c8048abbf2683a
licunzhi <2718272293@qq.com>
2019-05-07 10:37:41 +0800

    java如何创建ptt 动态数据渲染ppt的工程初始化

---
+++ java_ppt_demo/src/main/java/com/sakura/rain/Main.java
@@ -0,0 +1,18 @@
+package com.sakura.rain;
+
+/**
+ * @ClassName Main
+ * @Description 程序运行主函数
+ * @Author lcz
+ * @Date 2019/05/05 11:46
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * 基础数据准备
+        *
+        *
+        * */
+
+    }
+}
