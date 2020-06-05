f82b072d194cb5ea1c94d5b277d5d0c1f5ac68dd
licunzhi <2718272293@qq.com>
2020-06-05 10:11:19 +0800

    服务的三个注册中心搭建完成

---
+++ cloud-demo/eureka03/src/main/java/com/sakura/rain/EurekaApplication03.java
@@ -0,0 +1,20 @@
+package com.sakura.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName EurekaApplication01
+ * @function [eureka注册中心01]
+ * @notice 持久化操作不做业务上处理
+ * @Author lcz
+ * @Date 2020/06/05 09:21
+ */
+@SpringBootApplication
+@EnableEurekaServer // 开启eureka服务
+public class EurekaApplication03 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication03.class, args);
+    }
+}
