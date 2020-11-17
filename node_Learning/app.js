a5b55195a2059b92221c66fd1856669c1fc23a38
licunzhi <2718272293@qq.com>
2020-11-17 08:50:18 +0800

    node相关测试代码

--- node_Learning/app.js
+++ node_Learning/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testRouter = require('./routes/tests');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use(['/users', '/licunzhi'], usersRouter);
+app.use(['/tests', '/tests'], testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,10 +34,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    console.log(res.locals);
-    console.log(req.fresh);
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- sdc-demo/build/build.js
+++ sdc-demo/build/build.js
@@ -17,9 +17,29 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
+/* 开始构建控制台刷新展示 */
 const spinner = ora('building for production...')
 spinner.start()
 
+/*
+* rimraf说明： rimraf('some-thing', myCustomFS, callback)
+* 参数说明：
+* config.build.assetsRoot: dist 备注：名称也可以进行定义
+* config.build.assetsSubDirectory: static
+*
+* 构建过程需要删除之前构建的文件，不包括index.html
+*     主要是构建过程可能会打包出新的整合结果文件，如果不对static下的文件进行删除可能会导致文件堆积
+*     而index.html为入口文件，每次都会被覆盖
+*
+* 执行构建核心方法：webpack()
+*
+* function webpack(options, callback)
+*     validateSchema(webpackOptionsSchema, options) // schema格式校验 定义属性格式位置参考：/schemas/webpackOptionsSchema.json
+*     针对options的类型： Array | Object
+*     new WebpackOptionsDefaulter().process(options); // WebpackOptionsDefaulter构造器中的参数进行对比，不存在的使用默认值
+*
+* */
+console.log('构建需要移除的文件路径：', path.join(config.build.assetsRoot, config.build.assetsSubDirectory))
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
   webpack(webpackConfig, (err, stats) => {
